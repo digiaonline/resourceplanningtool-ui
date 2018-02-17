d569c719f03390be3ff2ce610238091b907b4f3a
Thanh Do <thanh.do@digia.com>
2018-02-17 20:33:26 +0200

    modify style for spacing
    Signed-off-by: Thanh Do <thanh.do@digia.com>

--- src/customers-container/components/CustomersContainer.js
+++ src/customers-container/components/CustomersContainer.js
@@ -14,10 +14,10 @@ const dummyCustomer = {
 class CustomersContainer extends Component {
   render() {
     return (
-      <div>
-        <div className={css.containerHeader}>
+      <div className={css.container}>
+        <div className={css.header}>
           <h3>Customer</h3>
-          <button type="button">
+          <button type="button" className={css.header__button}>
             <span id="logo" /> NEW CUSTOMER
           </button>
           {/* layout for Modal/form is yet to be implemented */}
@@ -26,23 +26,26 @@ class CustomersContainer extends Component {
             <h1>KEKEKEKEKE</h1>{' '}
           </Modal>
         </div>
-        <table>
+        <table className={css.container__table}>
           <thead>
             <tr>
-              <th>
+              <th className={css.row__name}>
                 {' '}
                 Customer <span id="sort" />{' '}
               </th>
-              <th> Website </th>
-              <th> Industry </th>
+              <th className={css.row__website}> Website </th>
+              <th className={css.row__industry}> Industry </th>
             </tr>
           </thead>
           <tbody>
             {[1, 2, 3, 4, 5, 6, 7].map(id => (
               <tr>
-                <td> {dummyCustomer.name} </td>
-                <td> {dummyCustomer.website} </td>
-                <td> {dummyCustomer.industry} </td>
+                <td className={css.row__name}> {dummyCustomer.name} </td>
+                <td className={css.row__website}> {dummyCustomer.website} </td>
+                <td className={css.row__industry}>
+                  {' '}
+                  {dummyCustomer.industry}{' '}
+                </td>
               </tr>
             ))}
           </tbody>
