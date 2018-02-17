ef6e187c80d0db0902508f01f7082c38d7d23fa7
Thanh Do <thanh.do@digia.com>
2018-02-17 11:34:49 +0200

    initiate (unstyled) layout of the main container (not include modal/form)

--- src/customers-container/components/CustomersContainer.js
+++ src/customers-container/components/CustomersContainer.js
@@ -1,16 +1,55 @@
 // @flow
 
 import React, {Component} from 'react';
+import Modal from 'react-modal';
 import css from './CustomersContainer.css';
+
+// sorry you are not dummy, my client
+const dummyCustomer = {
+  name: 'Dummy Customer',
+  website: 'http://dummy.site.fi',
+  industry: 'Industrialized Stupidity',
+};
 
 class CustomersContainer extends Component {
   render() {
     return (
       <div>
-        <h1> This should be the route for displaying customers. </h1>
+        <div className={css.containerHeader}>
+          <h3>Customer</h3>
+          <button type="button">
+            <span id="logo" /> NEW CUSTOMER
+          </button>
+          {/* layout for Modal/form is yet to be implemented */}
+          <Modal isOpen={false}>
+            {' '}
+            <h1>KEKEKEKEKE</h1>{' '}
+          </Modal>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                {' '}
+                Customer <span id="sort" />{' '}
+              </th>
+              <th> Website </th>
+              <th> Industry </th>
+            </tr>
+          </thead>
+          <tbody>
+            {[1, 2, 3, 4, 5, 6, 7].map(id => (
+              <tr>
+                <td> {dummyCustomer.name} </td>
+                <td> {dummyCustomer.website} </td>
+                <td> {dummyCustomer.industry} </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
-};
+}
 
 export default CustomersContainer;
