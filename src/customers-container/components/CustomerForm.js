a76141407dd197cef74bd2c6b3e20acd2534cc7f
Thanh Do <thanh.do@digia.com>
2018-02-19 11:12:40 +0200

    break the form to a seperate component
    Signed-off-by: Thanh Do <thanh.do@digia.com>

---
+++ src/customers-container/components/CustomerForm.js
@@ -0,0 +1,19 @@
+// @flow
+
+import React, {Component} from 'react';
+import Modal from 'react-modal';
+import css from './CustomersContainer.css';
+
+class CustomerForm extends Component {
+  render() {
+    return (
+      <Modal isOpen={true}>
+        <div>
+          <h3>KEKEKEKEKE</h3>
+        </div>
+      </Modal>
+    );
+  }
+}
+
+export default CustomerForm;
--- src/customers-container/components/CustomersContainer.js
+++ src/customers-container/components/CustomersContainer.js
@@ -1,8 +1,8 @@
 // @flow
 
 import React, {Component} from 'react';
-import Modal from 'react-modal';
 import css from './CustomersContainer.css';
+import CustomerForm from './CustomerForm';
 
 // sorry you are not dummy, my client
 const dummyCustomer = {
@@ -16,37 +16,27 @@ class CustomersContainer extends Component {
     return (
       <div className={css.container}>
         <div className={css.header}>
-          <h3>Customer</h3>
+          <div className={css.header__headline}>Customer</div>
           <button type="button" className={css.header__button}>
-            <span id="logo" /> NEW CUSTOMER
+            <i id="icon" /> NEW CUSTOMER
           </button>
           {/* layout for Modal/form is yet to be implemented */}
-          <Modal isOpen={false}>
-            {' '}
-            <h1>KEKEKEKEKE</h1>{' '}
-          </Modal>
+          <CustomerForm />
         </div>
         <table className={css.container__table}>
           <thead>
             <tr>
               <th className={css.row__name}>
                 {' '}
-                Customer <span id="sort" />{' '}
+                CUSTOMER <span id="sort" />{' '}
               </th>
-              <th className={css.row__website}> Website </th>
-              <th className={css.row__industry}> Industry </th>
+              <th className={css.row__website}> WEBSITE </th>
+              <th className={css.row__industry}> INDUSTRY </th>
             </tr>
           </thead>
           <tbody>
-            {[1, 2, 3, 4, 5, 6, 7].map(id => (
-              <tr>
-                <td className={css.row__name}> {dummyCustomer.name} </td>
-                <td className={css.row__website}> {dummyCustomer.website} </td>
-                <td className={css.row__industry}>
-                  {' '}
-                  {dummyCustomer.industry}{' '}
-                </td>
-              </tr>
+            {[1, 2, 3, 4, 5, 6, 7].map((id, index) => (
+              <CustomerRow key={index} />
             ))}
           </tbody>
         </table>
@@ -54,5 +44,15 @@ class CustomersContainer extends Component {
     );
   }
 }
+
+const CustomerRow = () => (
+  <tr>
+    <td className={css.row__name}> {dummyCustomer.name} </td>
+    <td className={css.row__website}>
+      <a href={dummyCustomer.website}> {dummyCustomer.website} </a>
+    </td>
+    <td className={css.row__industry}> {dummyCustomer.industry} </td>
+  </tr>
+);
 
 export default CustomersContainer;
