77991af4d1b23f5aca2f8ead9a2b0a0813952c62
Thanh Do <thanh.do@digia.com>
2018-02-19 16:18:27 +0200

    break down the list to smaller components, and put dummy data to a different file
    Signed-off-by: Thanh Do <thanh.do@digia.com>

---
+++ src/customers-container/components/dummyCustomers.js
@@ -0,0 +1,42 @@
+// @flow
+
+// sorry clients, you are not actually dumb
+const dummyCustomers = [
+  {
+    name: 'Dummy Customer',
+    website: 'http://dummy.site.fi',
+    industry: 'Industrialized Stupidity',
+  },
+  {
+    name: 'Dummy Customer',
+    website: 'http://dummy.site.fi',
+    industry: 'Industrialized Stupidity',
+  },
+  {
+    name: 'Dummy Customer',
+    website: 'http://dummy.site.fi',
+    industry: 'Industrialized Stupidity',
+  },
+  {
+    name: 'Dummy Customer',
+    website: 'http://dummy.site.fi',
+    industry: 'Industrialized Stupidity',
+  },
+  {
+    name: 'Dummy Customer',
+    website: 'http://dummy.site.fi',
+    industry: 'Industrialized Stupidity',
+  },
+  {
+    name: 'Dummy Customer',
+    website: 'http://dummy.site.fi',
+    industry: 'Industrialized Stupidity',
+  },
+  {
+    name: 'Dummy Customer',
+    website: 'http://dummy.site.fi',
+    industry: 'Industrialized Stupidity',
+  },
+];
+
+export default dummyCustomers;
