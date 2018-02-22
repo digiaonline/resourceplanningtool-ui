35a7b34b3df1730bfadb81793b61400155106df8
Thanh Do <thanh.do@digia.com>
2018-02-22 13:20:37 +0200

    add and modify layout for viewing a single person
    Signed-off-by: Thanh Do <thanh.do@digia.com>

--- src/people-container/dummyPeople.js
+++ src/people-container/dummyPeople.js
@@ -1,14 +1,15 @@
 const dummyPerson = {
-  id: 1,
-  imageUrl: 'https://goo.gl/images/3HJYGU',
+  id: 0,
+  imageUrl:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpKwTLaDvE1Rk4SBVI9vfs0ODtWGEpoj-ws4B1AL7op_TB637zMw',
   name: 'Erik Kieslowski',
-  title: 'Manager of communism',
+  title: 'Manager',
   startTimeInDigia: Date.now(), // mm/yy
   location: 'October Revolution',
   githubLink: 'https://en.wikipedia.org/wiki/String_theory',
   linkedinLink: 'https://en.wikipedia.org/wiki/String_theory',
   description:
-    'Harry Potter movies are better than books version, damn you Erik',
+    'Harry Potter movies are better than books version, damn you Erik. Better than Lord Of The Rings also. Triggered ?.',
   technologies: [
     {
       id: 1,
@@ -17,6 +18,10 @@ const dummyPerson = {
     {
       id: 2,
       level: 1, // [1-3]
+    },
+    {
+      id: 3,
+      level: 2, // [1-3]
     },
     {
       id: 3,
