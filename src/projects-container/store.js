712fbeaa75fbfb3249f52b8d1bf153af2c8e99e2
Mohammed Mansoor <mohamednmansour.90@gmail.com>
2018-03-08 12:27:35 +0200

    fetch techcologies from server and move function from project modal to srore
    Signed-off-by: Mohammed Mansoor <mohamednmansour.90@gmail.com>

--- src/projects-container/store.js
+++ src/projects-container/store.js
@@ -2,12 +2,14 @@
 
 import {observable, action} from 'mobx';
 import axios from 'axios';
+import form from './form';
 
 class ProjectsStore {
   @observable Data = [];
   @observable isOpen: Boolean = false;
   @observable projectId = null;
   @observable projectData = [];
+  @observable technologiesList = [];
 
   @action
   modalToggle = () => {
@@ -16,10 +18,10 @@ class ProjectsStore {
 
   @action
   fetchProject = async id => {
+    const URL = 'http://10.5.0.177:3002/skillz';
     this.projectId = id;
     const query = ` query { project (id: ${this.projectId}) ${this
       .projectQuery}`;
-    const URL = 'http://10.5.0.177:3002/skillz';
     try {
       const response = await axios.post(URL, query, {
         headers: {
@@ -48,34 +50,93 @@ class ProjectsStore {
     }
   };
 
+  fetchTechnologies = async () => {
+    const URL = 'http://10.5.0.177:3002/skillz';
+    try {
+      const response = await axios.post(URL, this.technologiesQuery, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      this.technologiesList = response.data.data.listTechnologies;
+    } catch (error) {
+      return [];
+    }
+  };
+
+  @action
+  addToMembers = select => {
+    if (!form.$('members').value.includes(select)) {
+      const values = form.$('members').value.concat(select);
+      form.$('members').set('value', values);
+    }
+  };
+
+  @action
+  removeAllMembers = () => {
+    form.$('members').set('value', []);
+  };
+
+  @action
+  removeMember = member => {
+    const Selected = form.$('members').value.filter(item => item !== member);
+    form.$('members').set('value', Selected);
+  };
+
+  @action
+  addToTechnologies = select => {
+    const allTechnologies = form.$('technologies').value.map(item => item.name);
+    console.log(allTechnologies);
+    if (!allTechnologies.includes(select)) {
+      const values = form.$('technologies').value.concat({name: select});
+      form.$('technologies').set('value', values);
+    }
+  };
+
+  @action
+  removeTechnologie = tech => {
+    const Selected = form
+      .$('technologies')
+      .value.filter(item => item.name !== tech);
+    form.$('technologies').set('value', Selected);
+  };
+
   projectsQuery = `query {
-  listProjects {
+listProjects {
+  id
+  name
+  description
+  githuburl
+  liveat
+  technologies {
     id
     name
-    description
-    githuburl
-    liveat
-    technologies {
-      name
-    }
   }
+}
 }`;
 
   projectQuery = `{
-    id
-		name
-    starttime
-    endtime
-    ongoing
-    liveat
-    githuburl
-    shortdescription
-    description
-    contactemail
-    customer { name }
-    technologies { name }
-    persons { name }
-	}
+  id
+  name
+  starttime
+  endtime
+  ongoing
+  liveat
+  githuburl
+  shortdescription
+  description
+  contactemail
+  customer { name }
+  technologies {id, name}
+  persons { name }
+}
+}`;
+
+  technologiesQuery = `query {
+listTechnologies {
+id
+name
+}
 }`;
 }
 
