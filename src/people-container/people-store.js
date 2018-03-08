ceba661922b5c410a191d6192636d0db63c188f4
Thanh Do <thanh.do@digia.com>
2018-03-08 12:40:15 +0200

    adding new query and function for creating a new person
    Signed-off-by: Thanh Do <thanh.do@digia.com>

--- src/people-container/people-store.js
+++ src/people-container/people-store.js
@@ -2,7 +2,7 @@
 
 import {observable, action} from 'mobx';
 import axios from 'axios';
-import {FETCH_PEOPLE_QUERY} from './queries';
+import {FETCH_PEOPLE_QUERY, getCreatePersonQuery} from './queries';
 
 class PeopleStore {
   // typechecking and initial value for list of people
@@ -32,6 +32,28 @@ class PeopleStore {
       this.people = responseData.listPersons;
     } catch (error) {
       console.log('cant fetch people', error);
+    }
+  };
+
+  @action
+  createPeople = async (personInfo: Object) => {
+    try {
+      const CREATE_PERSON_QUERY = getCreatePersonQuery(
+        personInfo.name,
+        personInfo.description,
+        personInfo.picture,
+        personInfo.startdate,
+        personInfo.email,
+        personInfo.title,
+        personInfo.location,
+        personInfo.githuburl,
+        personInfo.linkedinurl
+      );
+      await this.makeHttpRequest(CREATE_PERSON_QUERY);
+      alert('create person successfully');
+      this.fetchPeople();
+    } catch (error) {
+      console.log('cant create person', error);
     }
   };
 
--- src/people-container/queries.js
+++ src/people-container/queries.js
@@ -1,3 +1,5 @@
+// @flow
+
 export const FETCH_PEOPLE_QUERY = `query {
 	listPersons {
         description
@@ -16,3 +18,70 @@ export const FETCH_PEOPLE_QUERY = `query {
         startdate
     }
 }`;
+
+export const getCreatePersonQuery = (
+  name: String,
+  description: String,
+  picture: String,
+  startdate: Date,
+  email: String,
+  title: String,
+  location: String,
+  githuburl: String,
+  linkedinurl: String
+) => {
+  return `mutation {
+		createPerson(
+			name: "${name}", description: "${description}", picture: "${picture}",
+			startdate: "${startdate.getTime()}", email: "${email}", title: "${title}",
+			location: "${location}", githuburl: "${githuburl}", linkedinurl: "${linkedinurl}"
+		) {
+			id
+		}
+	}`;
+};
+
+export const getUpdatePersonQuery = (
+  name: String,
+  description: String,
+  picture: String,
+  startdate: Date,
+  email: String,
+  title: String,
+  location: String,
+  githuburl: String,
+  linkedinurl: String
+) => {
+  return `mutation {
+		updatePerson(
+			name: "${name}", description: "${description}", picture: "${picture}",
+			startdate: "${startdate.getTime()}", email: "${email}", title: "${title}",
+			location: "${location}", githuburl: "${githuburl}", linkedinurl: "${linkedinurl}"
+		)
+	}`;
+};
+
+export const getCreateSkillsQuery = (
+  skills: [
+    {
+      level: Number,
+      name: String
+    }
+  ]
+) => {
+  return `mutation {
+		${skills.map(
+    (
+      skill,
+      index
+    ) => `skill${index}: createSkill(name: "${skill.name}", level: ${skill.level}) {
+			id
+		}\n`
+  )}
+	}`;
+};
+
+export const getAddSkillForPersonQuery = (
+  personId: Number,
+  skillsId: [Number]
+) => {};
