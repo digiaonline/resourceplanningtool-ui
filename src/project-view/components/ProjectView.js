ce44975870bef0eaadbfb102fca43a2528c40e62
Chi Nguyen <ext-chi.nguyen@elisa.fi>
2018-04-19 15:00:24 +0300

    remove random html5 encode spacing

--- src/project-view/components/ProjectView.js
+++ src/project-view/components/ProjectView.js
@@ -57,7 +57,7 @@ class ProjectView extends Component {
       <div className={css.project__view}>
         <Link className={css.back__button} to="/projects">
           <img src={backIcon} alt="back" />
-          <span>&nbsp;Back</span>
+          <span>Back</span>
         </Link>
         <div className={css.project__heading}>
           <div className={css.project__title}>{Data.name}</div>
